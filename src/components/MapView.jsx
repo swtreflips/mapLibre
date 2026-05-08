324de619a0e2fa748fa4e4766a66ea541ec5f1e9
swtreflips <hernandez73k@gmail.com>
2026-05-07 22:25:07 -0500

    initial commit

---
+++ src/components/MapView.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef } from 'react'
+import maplibregl from 'maplibre-gl'
+import 'maplibre-gl/dist/maplibre-gl.css'
+import './MapView.css'
+
+const STYLE_URL = 'https://tiles.openfreemap.org/styles/liberty'
+const INITIAL_CENTER = [0, 20]
+const INITIAL_ZOOM = 1.5
+
+// Min zoom where exactly one world copy fills the container width
+// (vector tiles are 512px, so world width at zoom z is 512 * 2^z).
+const computeMinZoom = (width) => Math.log2(width / 512)
+
+export default function MapView() {
+  const containerRef = useRef(null)
+  const mapRef = useRef(null)
+
+  useEffect(() => {
+    const initialMinZoom = computeMinZoom(containerRef.current.clientWidth)
+
+    const map = new maplibregl.Map({
+      container: containerRef.current,
+      style: STYLE_URL,
+      center: INITIAL_CENTER,
+      zoom: Math.max(INITIAL_ZOOM, initialMinZoom),
+      minZoom: initialMinZoom,
+    })
+
+    map.addControl(new maplibregl.NavigationControl(), 'top-right')
+
+    map.on('resize', () => {
+      map.setMinZoom(computeMinZoom(map.getContainer().clientWidth))
+    })
+
+    mapRef.current = map
+
+    return () => {
+      map.remove()
+      mapRef.current = null
+    }
+  }, [])
+
+  return <div ref={containerRef} className="map-container" />
+}
